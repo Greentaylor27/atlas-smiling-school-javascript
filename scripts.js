b7b5c5caed9194ee4b809c023eb3c1bc0915e492
Greentaylor27 <7196@holbertonstudents.com>
2024-11-30 17:14:58 -0600

    Testing a few things out

--- scripts.js
+++ scripts.js
@@ -3,33 +3,28 @@ $(document).ready(function() {
     let page = $('body').data('page');
 
     if (page === 'page1') {
-        console.log("This is the HomePage");
+        console.log('This is the Homepage');
+
         $.ajax ({
-            url: "https://smileschool-api.hbtn.info/quotes",
+            url: 'https://smileschool-api.hbtn.info/quotes',
             type: 'GET',
-            dataType: 'json',
-            success: function(data) {
+            success: (data) => {
                 console.log(data);
-                data.forEach(person => {
-                    console.log('ID: ' + person.id);
-                    console.log('Pic_URL: ' + person.pic_url);
-                    console.log('Name: ' + person.name);
-                    console.log('Title: ' + person.title);
-                    console.log('Text: ' + person.text);
-                });
-            },
-            error: function(error) {
-                console.error('There is an error', error);
+                console.log(data[0].name);
+
+                
             }
-        });
+        })
     }
+
     else if (page === 'page2') {
-        console.log("This is Pricing");
+        console.log('This is the Pricing page');
     }
+
     else if (page === 'page3') {
-        console.log("This is Courses");
+        console.log('This is the Courses page');
     }
     else {
-        console.error("There is nopage");
+        console.error(error);
     }
-})
+});
