7e3b91df5da90d4c2eaaa02f4946ef062aaf1f93
Greentaylor27 <7196@holbertonstudents.com>
2024-12-02 16:53:17 -0600

    Got everything dynamically allocating the dom

--- scripts.js
+++ scripts.js
@@ -52,23 +52,22 @@ $(document).ready(function() {
             url: 'https://smileschool-api.hbtn.info/latest-videos',
             type: 'GET',
             success: (data) => {
-                console.log(data);
                 for (let i = 0; i < 12; i++) {
-                    //const data = video[i % video.length];
+                    const video = data[i % data.length];
 
-                    /* $(`.latestVideo${i + 1},
-                        .cardTitle${i + 1},
-                        .subTitle${i + 1},
-                        .cardAuthor${i + 1},
-                        .videoDuration${i + 1},
-                        .profile_Pic${i + 1}`).empty();
-*/
-                    // $(`.popularVideo${i + 1}`).attr('src', data.thumb_url);
-                    // $(`.cardTitle${i + 1}`).append(data.title);
-                    // $(`.cardAuthor${i + 1}`).append(data.author);
-                    // $(`.videoDuration${i + 1}`).append(data.duration);
-                    // $(`.profile_Pic${i + 1}`).attr('src', data.author_pic_url);
-                    // $(`.subTitle${i + 1}`).append(data['sub-title']);
+                    $(`.latestVideo${i + 1},
+                        .latestTitle${i + 1},
+                        .latestSub${i + 1},
+                        .latestAuthor${i + 1},
+                        .latestDuration${i + 1},
+                        .latestProfile${i + 1}`).empty();
+
+                    $(`.latestVideo${i + 1}`).attr('src', video.thumb_url);
+                    $(`.latestTitle${i + 1}`).append(video.title);
+                    $(`.latestAuthor${i + 1}`).append(video.author);
+                    $(`.latestDuration${i + 1}`).append(video.duration);
+                    $(`.latestProfile${i + 1}`).attr('src', video.author_pic_url);
+                    $(`.latestSub${i + 1}`).append(video['sub-title']);
                 
                 }
             }
@@ -100,10 +99,6 @@ $(document).ready(function() {
                 }
             }
         });
-
-        function showLoader(){
-
-        }
     }
 
     else if (page === 'page3') {
