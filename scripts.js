49e18f321557ceb73928034a402ca68e048e47a3
Greentaylor27 <7196@holbertonstudents.com>
2024-12-02 14:56:30 -0600

    Over doing it a bit went too far with this branch switching to main

--- scripts.js
+++ scripts.js
@@ -3,22 +3,107 @@ $(document).ready(function() {
     let page = $('body').data('page');
 
     if (page === 'page1') {
-        console.log('This is the Homepage');
+
+    $.ajax ({
+        url: 'https://smileschool-api.hbtn.info/quotes',
+        type: 'GET',
+        success: (data) => {
+                    
+            for (let i = 0; i < 3; i++) {
+                const quote = data[i] || data[0];
+    
+                $(`.carousel-name${i + 1}, .singleQuote${i + 1}, .singleTitle${i + 1}`).empty();
+    
+                $(`.profilePic${i + 1}`).attr('src', quote.pic_url);
+                $(`.carousel-name${i + 1}`).append(quote.name);
+                $(`.singleQuote${i + 1}`).append(quote.text);
+                $(`.singleTitle${i + 1}`).append(quote.title);
+            }
+        }
+    });
+
+        $.ajax({
+            url: 'https://smileschool-api.hbtn.info/popular-tutorials',
+            type: 'GET',
+            success: (video) => {
+
+                for (let i = 0; i < 12; i++) {
+                    const data = video[i % video.length];
+
+                    $(`.popularVideo${i + 1},
+                        .cardTitle${i + 1},
+                        .subTitle${i + 1},
+                        .cardAuthor${i + 1},
+                        .videoDuration${i + 1},
+                        .profile_Pic${i + 1}`).empty();
+
+                    $(`.popularVideo${i + 1}`).attr('src', data.thumb_url);
+                    $(`.cardTitle${i + 1}`).append(data.title);
+                    $(`.cardAuthor${i + 1}`).append(data.author);
+                    $(`.videoDuration${i + 1}`).append(data.duration);
+                    $(`.profile_Pic${i + 1}`).attr('src', data.author_pic_url);
+                    $(`.subTitle${i + 1}`).append(data['sub-title']);
+                
+                }
+            }
+        });
+
+        $.ajax({
+            url: 'https://smileschool-api.hbtn.info/latest-videos',
+            type: 'GET',
+            success: (data) => {
+                console.log(data);
+                for (let i = 0; i < 12; i++) {
+                    //const data = video[i % video.length];
+
+                    /* $(`.latestVideo${i + 1},
+                        .cardTitle${i + 1},
+                        .subTitle${i + 1},
+                        .cardAuthor${i + 1},
+                        .videoDuration${i + 1},
+                        .profile_Pic${i + 1}`).empty();
+*/
+                    // $(`.popularVideo${i + 1}`).attr('src', data.thumb_url);
+                    // $(`.cardTitle${i + 1}`).append(data.title);
+                    // $(`.cardAuthor${i + 1}`).append(data.author);
+                    // $(`.videoDuration${i + 1}`).append(data.duration);
+                    // $(`.profile_Pic${i + 1}`).attr('src', data.author_pic_url);
+                    // $(`.subTitle${i + 1}`).append(data['sub-title']);
+                
+                }
+            }
+        })
+
+        function showLoader(event) {
+
+        }
+    }
+
+    else if (page === 'page2') {
+        console.log('This is the Pricing page');
 
         $.ajax ({
             url: 'https://smileschool-api.hbtn.info/quotes',
             type: 'GET',
             success: (data) => {
-                console.log(data);
-                console.log(data[0].name);
 
+                $('.carousel-name1, .singleQuote1, .singleTitle1').empty();
+                $('.carousel-name2, .singleQuote2, .singleTitle2').empty();
+                $('.carousel-name3, .singleQuote3, .singleTitle3').empty();
                 
+                for (let i = 0; i < 3; i++) {
+                    const quote = data[i] || data[0];
+                    $(`.profilePic${i + 1}`).attr('src', quote.pic_url);
+                    $(`.carousel-name${i + 1}`).append(quote.name);
+                    $(`.singleQuote${i + 1}`).append(quote.text);
+                    $(`.singleTitle${i + 1}`).append(quote.title);
+                }
             }
-        })
-    }
+        });
 
-    else if (page === 'page2') {
-        console.log('This is the Pricing page');
+        function showLoader(){
+
+        }
     }
 
     else if (page === 'page3') {
